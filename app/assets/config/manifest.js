74cfcd4cff90bbc290faa0e82d05043a19e8b8ab
nvdong123 <donghkti10@gmail.com>
2025-01-10 08:58:37 +0700

    update

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,4 @@
+//= link_tree ../images
+//= link_directory ../application. scss
+//= link_directory ../application. js
+//= link_directory ../stylesheets/actiontext .css
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,7 @@ import "controllers"
 
 import "trix"
 import "@rails/actiontext"
+import "bootstrap"
+//= require jquery3
+//= require popper
+//= require bootstrap
